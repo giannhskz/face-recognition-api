cf4ad4cdb3b1a4c64d4f7326e1670009cbcae2ef
giannhskz <giannhskz@gmail.com>
2022-09-28 04:47:29 +0300

    db up

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-silhouetted-39862",
-    user: "postgres",
-    password: "admin",
-    database: "smartdb",
+    host: "process.env.DATABASE_URL",
+    ssl: true,
   },
 });
 
