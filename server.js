4f5f577c4b8f0842803117e0402ee065d9e18fe5
giannhskz <giannhskz@gmail.com>
2022-09-29 00:51:49 +0300

    'h'

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const signin = require("./controllers/signin");
 const profile = require("./controllers/profile");
 const image = require("./controllers/image");
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: "process.env.DATABASE_URL",
+    connectionString:
+      "postgres://ewrrxwsfjgaglm:ac35372127dcb76e7d0733ed8fe4c9bf4a6dc7488f0216bc71f498ec206d98ef@ec2-34-207-12-160.compute-1.amazonaws.com:5432/dc6jekbcodcdgf",
     ssl: {
       rejectUnauthorized: false,
     },
